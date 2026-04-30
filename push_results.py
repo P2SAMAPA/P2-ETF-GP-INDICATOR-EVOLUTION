f8ffbc3cc7b5ff3af1784c3eada5e4a8dce01e0d
P2SAMAPA <pranabpattanaik@gmail.com>
2026-04-30 10:08:40 +0800

    Create push_results.py

---
+++ push_results.py
@@ -0,0 +1,15 @@
+"""
+Push results to Hugging Face dataset.
+"""
+
+import json
+from huggingface_hub import HfFileSystem
+import config
+
+def push_daily_result(result_dict):
+    filename = f"gp_indicator_{config.TODAY}.json"
+    fs = HfFileSystem(token=config.HF_TOKEN)
+    json_str = json.dumps(result_dict, indent=2)
+    with fs.open(f"datasets/{config.HF_OUTPUT_REPO}/{filename}", "w") as f:
+        f.write(json_str)
+    print(f"Results pushed to {config.HF_OUTPUT_REPO}/{filename}")
