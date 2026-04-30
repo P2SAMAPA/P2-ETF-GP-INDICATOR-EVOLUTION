9bc1005bd87253bfcea0b3383210f20cab6fb0ff
P2SAMAPA <pranabpattanaik@gmail.com>
2026-04-30 10:09:04 +0800

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,7 @@
+"""
+Utility functions for GP evolution.
+"""
+
+def simplify_formula(formula_str):
+    """Remove unnecessary spaces and newlines for display."""
+    return formula_str.replace('\n', ' ').strip()
